ee9c7f02dc648f0da3a436bb97d8dd5ca4f6d404
Benjamin Burga <burgabenjamin872@gmail.com>
2024-06-03 21:14:09 -0700

    Update code

---
+++ project/scripts/project.js
@@ -0,0 +1,9 @@
+const currentYearElement = document.getElementById("current-year");
+const currentYear = new Date().getFullYear();
+
+currentYearElement.textContent = currentYear;
+
+const lastModifiedElement = document.getElementById("last-modified");
+const lastModified = document.lastModified;
+
+lastModifiedElement.textContent = `Last modified: ${lastModified}`
